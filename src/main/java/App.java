1307575559d3753bc36f766bf22b1e2c0549d517
sasoara <b.sarah2@hotmail.com>
2019-06-09 23:12:22 +0200

    loading fxml file works

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,30 +3,42 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+
 
 public class App extends Application {
 
+    public App() {
+        System.out.println("App class");
+    }
+
+    /**
+     * The {@link #launch} method sets up my
+     * <code>Main</code> class as a JavaFX application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(App.class, args);
+    }
+
+    /**
+     * is created for the test class #AppTest
+     * @return
+     */
     public String greeting() {
         return "hallo";
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
-//        Parent page = FXMLLoader.<Parent>load(getClass().getResource("main.fxml").toExternalForm());
+    public void start(Stage primaryStage) throws IOException{
 
         primaryStage.setTitle("Hotel self check-in");
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass()
                         .getClassLoader()
-                        .getResource("resources/fxml/Vorlage.fxml"));
+                        .getResource("fxml/main_window.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
     }
-
-    public static void main(String[] args) {
-        launch(App.class, args);
-    }
-
 }
