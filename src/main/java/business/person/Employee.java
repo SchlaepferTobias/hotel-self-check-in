444d98a1ac79e6676fb77a820a6cfcfde49a20a6
Tobias <schlapferto@gmail.com>
2019-06-25 09:57:21 +0200

    Java dock

--- src/main/java/business/person/Employee.java
+++ src/main/java/business/person/Employee.java
@@ -10,6 +10,16 @@ public class Employee extends Person {
     private int employeeNumber;
     private Date birthDate;
 
+    /**
+     *Filtert die wichtigen informationen aus den mitgegebenen werten und speichert diese.
+     *
+     * @param name Name des Mitarbeiters
+     * @param firstname Vorname des Mitarbeiters
+     * @param address Adresse des Mitarbeiters
+     * @param position Position des Mitarbeiters
+     * @param employeeNumber Nummer des Mitarbeiters
+     * @param birthDate Geburtsdatum des Mitarbeiters
+     */
     public Employee(String name, String firstname, Address address, String position, int employeeNumber, Date birthDate) {
         super(name, firstname, address);
         this.position = position;
@@ -17,14 +27,29 @@ public class Employee extends Person {
         this.birthDate = birthDate;
     }
 
+    /**
+     * Giebt die Position des Mitarbeiters zurück.
+     *
+     * @return position
+     */
     public String getPosition() {
         return position;
     }
 
+    /**
+     * Giebt die Mitarbeiter nummer zurück.
+     *
+     * @return employeeNumber
+     */
     public int getEmployeeNumber() {
         return employeeNumber;
     }
 
+    /**
+     * giebt das Geburtzdatum des Mitarbeiters zurück.
+     *
+     * @return birthDate
+     */
     public Date getBirthDate() {
         return birthDate;
     }
--- src/main/java/business/person/Guest.java
+++ src/main/java/business/person/Guest.java
@@ -7,16 +7,35 @@ public class Guest extends Person {
     private String mobile;
     private String email;
 
+    /**
+     * Filtert die wichtigen informationen aus den mitgegebenen werten und speichert diese.
+     *
+     * @param name Familienname des Gastes
+     * @param firstname Vorname des Gastes
+     * @param address Adresse des Gastes
+     * @param mobile MobileNr des Gastes
+     * @param email Email adresse des Gastes
+     */
     public Guest(String name, String firstname, Address address, String mobile, String email) {
         super(name, firstname, address);
         this.mobile = mobile;
         this.email = email;
     }
 
+    /**
+     * Giebt die mobilnummer des Gastes zurück
+     *
+     * @return mobile
+     */
     public String getMobile() {
         return mobile;
     }
 
+    /**
+     * Giebt die Email adresse des Gastes zurück
+     *
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
